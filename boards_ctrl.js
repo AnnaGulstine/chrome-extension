6c93870c57ab725f1785a7d4dde7c51d5a06dfe6
AnnaGulstine <annagulstine@gmail.com>
2016-05-09 21:05:44 -0500

    Added new post request for addPin function

--- boards_ctrl.js
+++ boards_ctrl.js
@@ -1,4 +1,4 @@
-/* global angular */
+/* global angular, chrome */
 
 (function() {
   angular.module("app").controller("boardsController", function($scope, $http) {
@@ -6,6 +6,13 @@
       $http.get("http://localhost:3000/api/v1/boards/get_id").then(function(response) {
         $scope.boards = response.data;
       });
+
+      // chrome.tabs.getSelected(null, function(tab) {
+      //   console.log('init get tab', tab);
+      // });
+      // chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+      //   console.log(tabs[0].url);
+      // });
     };
 
     // $scope.addPin = function(text) {
@@ -18,13 +25,17 @@
     //   });
     // };
 
-    $scope.addBoard = function(address) {
-      var newBoard = {
-        address: address
-      };
-      $http.post("http://localhost:3000/api/v1/boards/get_id", newBoard).then(function(response) {
-        console.log(response);
-        $scope.boards.push(newBoard);
+    $scope.addPin = function(text, board_id) {
+      chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+        var params = {
+          text: text,
+          url: tabs[0].url,
+          board_id: board_id
+        };
+        $http.post("http://localhost:3000/api/v1/pins.json", params).then(function(response) {
+          console.log(response);
+          // $scope.boards.push(newBoard);
+        });
       });
     };
   });
