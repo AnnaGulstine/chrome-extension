b23823273781d07303245b0b1ee682cc6fc6c124
AnnaGulstine <annagulstine@gmail.com>
2016-05-09 19:51:39 -0500

    Added post method for adding new pins

--- boards_ctrl.js
+++ boards_ctrl.js
@@ -3,14 +3,29 @@
 (function() {
   angular.module("app").controller("boardsController", function($scope, $http) {
     $scope.setup = function() {
-      $http.get("http://localhost:3000/api/v1/boards/get_id.json").then(function(response) {
+      $http.get("http://localhost:3000/api/v1/boards/get_id").then(function(response) {
         $scope.boards = response.data;
       });
     };
 
-    $scope.savePage = function() {
-      $http.post("http://localhost:3000/api/v1/pins.json");
-      $scope.message = "hello";
+    // $scope.addPin = function(text) {
+    //   var newPin = {
+    //     text: text
+    //   };
+    //   $http.post("http://localhost:3000/api/v1/pins", newPin).then(function(response) {
+    //     console.log(response);
+    //     $scope.boards.push(newPin);
+    //   });
+    // };
+
+    $scope.addBoard = function(address) {
+      var newBoard = {
+        address: address
+      };
+      $http.post("http://localhost:3000/api/v1/boards/get_id", newBoard).then(function(response) {
+        console.log(response);
+        $scope.boards.push(newBoard);
+      });
     };
   });
 })();
