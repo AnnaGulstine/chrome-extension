7fad648a467f23c8e192a6a141a14246668a2de4
AnnaGulstine <annagulstine@gmail.com>
2016-05-14 20:04:05 -0500

    Added category_id to addPin function

--- boards_ctrl.js
+++ boards_ctrl.js
@@ -3,18 +3,20 @@
 (function() {
   angular.module("app").controller("boardsController", function($scope, $http) {
     $scope.setup = function() {
-      $http.get("http://localhost:3000/api/v1/boards/get_id").then(function(response) {
+      $http.get("http://localhost:3000/api/v1/boards").then(function(response) {
         $scope.boards = response.data;
+        // $scope.category_id = response.data.pins;
       });
     };
 
-    $scope.addPin = function(board_id, name, text) {
+    $scope.addPin = function(category_id, board_id, name, text) {
       chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
         var params = {
           url: tabs[0].url,
           board_id: board_id,
-          name: name,
-          text: text
+          name: name || tabs[0].title,
+          text: text,
+          category_id: category_id
         };
         $http.post("http://localhost:3000/api/v1/pins.json", params).then(function(response) {
           console.log(response);
